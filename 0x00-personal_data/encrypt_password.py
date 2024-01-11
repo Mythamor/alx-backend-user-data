43e44c6f747bd87a286f1195640ff7d4cf0548ae
Mythamor <bethmithamo.bm@gmail.com>
2024-01-12 01:24:51 +0300

    Encrypt password with bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,14 +13,13 @@ def hash_password(password: str) -> ByteString:
     Hashes a password with bcrypt
     """
     hashed_password = bcrypt.hashpw(password.encode('utf-8'),
-                                bcrypt.gensalt())
+                                    bcrypt.gensalt())
 
     return hashed_password
 
 
-def is_valid(hashed_password: bytes, password:str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Validate hashed_password matches provided password
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
