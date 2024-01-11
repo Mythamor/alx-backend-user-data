428aabb19997c2409bf6b4c6fb1f1b7f6e0c9b70
Mythamor <bethmithamo.bm@gmail.com>
2024-01-11 21:38:16 +0300

    Obscuring user data with regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Module: filtered_logger.py
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
