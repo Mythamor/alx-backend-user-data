5e9c4c50cc381f0472980dd170e6eed7d258f629
Mythamor <bethmithamo.bm@gmail.com>
2024-01-17 00:51:49 +0300

    Implementing Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,5 +10,20 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """
     Inherits from Auth
+        Methods:
+                extract_base64_authorization_header
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Implements Base64 on Authorization header
+        """
+        if authorization_header is None or not\
+                isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ')[1]
