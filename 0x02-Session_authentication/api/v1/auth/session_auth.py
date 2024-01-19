3d710af538069fc391a7e6cc406241f0df680d5c
Mythamor <bethmithamo.bm@gmail.com>
2024-01-19 16:04:48 +0300

    Update the session_auth file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
+
 """
 Module: session_auth.py
-    Authenticates the session
+    Authenticates the session.
+
+    Provides a class for session-based authentication,
+     extending the base Auth class.
+
+    Classes:
+    - SessionAuth: Handles session-based authentication.
+
+    Methods:
+    - __init__(self): Initializes a new instance of SessionAuth.
+    - current_user(self, request=None): Retrieves the current user
+      based on the session.
+    - create_session(self, user_id=None): Creates a new session
+      for the given user ID.
+    - destroy_session(self, request=None): Destroys the current session.
 """
 
-from ap1.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """
-    Class for Session_based authentication
-        Methods:
+    Class for Session-based authentication.
+
+    Inherits from the base Auth class and provides methods
+    for session-based authentication.
+
+    Methods:
+    - __init__(self): Initializes a new instance of SessionAuth.
+    - current_user(self, request=None):
+      Retrieves the current user based on the session.
+    - create_session(self, user_id=None):
+      Creates a new session for the given user ID.
+    - destroy_session(self, request=None):
+      Destroys the current session.
     """
     pass
