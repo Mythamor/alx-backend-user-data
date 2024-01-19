d1b805457567e6ea04f8fcb65f9058b1d4c38c3e
Mythamor <bethmithamo.bm@gmail.com>
2024-01-19 14:04:06 +0300

    Setting up session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,8 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request, g
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,11 +16,12 @@ auth = None
 auth_type = os.getenv('AUTH_TYPE')
 
 # Check for auth_type
-if auth_type == 'basic_auth':
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-else:
-    auth = Auth()
 
 
 @app.before_request
@@ -35,18 +34,15 @@ def before_request():
                    '/api/v1/forbidden/']
 
     if auth is None:
-        return
+        pass
     else:
         setattr(request, "current_user", auth.current_user(request))
 
         if auth.require_auth(request.path, routes_list):
-            return
-
-        if auth.authorization_header(request) is None:
-            abort(401)
-
-        if auth.current_user(request) is None:
-            abort(403)
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
