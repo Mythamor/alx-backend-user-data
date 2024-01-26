0520909950e238c5a53ac24d2882d1de642ba3e3
Mythamor <bethmithamo.bm@gmail.com>
2024-01-27 00:24:13 +0300

    Set up profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,21 @@ def logout():
         abort(403)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """
+    Get the profile data of he user
+    """
+    # Get user from session ID
+    session_id = request.cookies.get('session_id')
+
+    # Find user from session ID
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({f"email": "{email}"}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
